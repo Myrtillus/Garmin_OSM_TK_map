edef7a9ff6e92ea7e6f10e9fc7b9ace5efd4f9a5
Antti Nissinen <antti@myrtillus.net>
2016-06-21 21:56:12 +0000

    OUlun laskenta mukaan... jotain pitaa muuttaa siten, etta tampereen ja oulun kartta nakyisivat yhtaikaa garminin valikossa

--- generate_map.sh
+++ generate_map.sh
@@ -9,8 +9,11 @@ set -o nounset
 
 cd /home/nissiant/Garmin_OSM_TK_map
 
+# TAMPERE
+#################
+
+
 # Splitting the file
-#osmosis/bin/osmosis --read-pbf file=/var/tmp/osm/finland-latest.osm.pbf --bounding-box left=23.20 bottom=61.20 right=24.6 top=62.00 --write-pbf tampere_region_final.osm.pbf
 osmosis/bin/osmosis --read-pbf file=/var/tmp/osm/finland-latest.osm.pbf --bounding-box left=23 bottom=60.8 right=24.9 top=62.00 --write-pbf tampere_region_final.osm.pbf
 
 # Generate the map file
@@ -21,6 +24,25 @@ mv gmapsupp.img OSM_TK_MTB_map.img
 sudo chown www-data:www-data OSM_TK_MTB_map.img
 sudo mv -f OSM_TK_MTB_map.img /var/www
 
+# OULU
+#################
+
+
+# Splitting the file
+osmosis/bin/osmosis --read-pbf file=/var/tmp/osm/finland-latest.osm.pbf --bounding-box left=24.4 bottom=64.6 right=26.5 top=65.33 --write-pbf oulu_region_final.osm.pbf
+
+# Generate the map file
+java -jar -Xmx1000m mkgmap.jar --max-jobs --gmapsupp --latin1 --tdbfile --nsis --mapname=88889999 --description=OSM_MTB_Oulu_region --family-id=8889 --series-name="OSM MTB Oulu region" --style-file=TK/ --precomp-sea=sea.zip --generate-sea --route --drive-on=detect,right --process-destination --process-exits --index --bounds=bounds.zip --location-autofill=is_in,nearest --x-split-name-index --housenumbers --remove-ovm-work-files oulu_region_final.osm.pbf TK.typ
+
+# copy the map file to /var/www for downloading
+mv gmapsupp.img OSM_TK_MTB_map_Oulu.img
+sudo chown www-data:www-data OSM_TK_MTB_map_Oulu.img
+sudo mv -f OSM_TK_MTB_map_Oulu.img /var/www
+
+
+#################################
+
 # Clean the directory
 rm osmmap.* *.img *.pbf osmmap_license.txt
+
 
