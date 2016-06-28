ec7bfcc5d698cadf7ae067416fdf06d51e3e7d3a
Antti Nissinen <antti@myrtillus.net>
2016-06-28 05:55:31 +0000

    Karttojen nimia lyhennetty minimiin, jotta ne nakyisivat samaan aikaan gps:n valikossa. FamilyID:t muutettu yksilolliseksi, jolla koetetaan myos saada kartat nakymaan samaan aikaan karttaruudulla siten, etta Tampere ei peittoa Oulua

--- generate_map.sh
+++ generate_map.sh
@@ -29,22 +29,22 @@ wget -O tampere.osm "http://overpass.osm.rambler.ru/cgi/xapi_meta?*[bbox=22.8,61
 
 java -jar -Xmx1000m splitter.jar tampere.osm --precomp-sea=sea.zip --geonames-file=cities15000.zip --max-areas=4096 --max-nodes=3000000 --wanted-admin-level=8
 
-# Fix the names in the template.args file descriptions
+# Fix the names in the template.args file descriptions, MAX 20 CHARACTERS
 
-python fix_names.py OSM_MTB_Tampere_region
+python fix_names.py TK_MTB_Tampere
 
 
-# Create the gmapsupp map file, NOTE THE MAPNAME HAS TO UNIQUE
+# Create the gmapsupp map file, NOTE THE MAPNAME HAS TO UNIQUE, FAMILY ID IS ALSO UNIQUE
 
-java -jar -Xmx1000m mkgmap.jar --max-jobs --gmapsupp --latin1 --tdbfile --nsis --mapname=88880001 --family-id=8888 --series-name="OSM MTB Tampere region" --style-file=TK/ --precomp-sea=sea.zip --generate-sea --route --drive-on=detect,right --process-destination --process-exits --index --bounds=bounds.zip --location-autofill=is_in,nearest --x-split-name-index --housenumbers --remove-ovm-work-files -c template.args TK.typ
+java -jar -Xmx1000m mkgmap.jar --max-jobs --gmapsupp --latin1 --tdbfile --nsis --mapname=88880001 --family-id=8800 --series-name="TK_MTB_Tampere" --style-file=TK/ --precomp-sea=sea.zip --generate-sea --route --drive-on=detect,right --process-destination --process-exits --index --bounds=bounds.zip --location-autofill=is_in,nearest --x-split-name-index --housenumbers --remove-ovm-work-files -c template.args TK.typ
 
 #java -jar -Xmx1000m mkgmap.jar --max-jobs --gmapsupp --latin1 --tdbfile --nsis --mapname=88880001 --family-id=8888 --style-file=TK/ --precomp-sea=sea.zip --generate-sea --route --drive-on=detect,right --process-destination --process-exits --index --bounds=bounds.zip --location-autofill=is_in,nearest --x-split-name-index --housenumbers --remove-ovm-work-files -c template.args TK.typ
 
 # copy the map file to /var/www for downloading and clean up the directory
 
-mv gmapsupp.img OSM_TK_MTB_map_Tampere.img
-sudo chown www-data:www-data OSM_TK_MTB_map_Tampere.img
-sudo mv -f OSM_TK_MTB_map_Tampere.img /var/www
+mv gmapsupp.img tk_tre.img
+sudo chown www-data:www-data tk_tre.img
+sudo mv -f tk_tre.img /var/www
 
 
 
@@ -68,18 +68,18 @@ wget -O oulu.osm "http://overpass.osm.rambler.ru/cgi/xapi_meta?*[bbox=24.5 ,64.7
 
 java -jar -Xmx1000m splitter.jar oulu.osm --precomp-sea=sea.zip --geonames-file=cities15000.zip --max-areas=2048 --max-nodes=1000000 --wanted-admin-level=8
 
-# Fix the names in the template.args file descriptions
+# Fix the names in the template.args file descriptions, MAX 20 CHARACTERS
 
-python fix_names.py OSM_MTB_Oulu_region
+python fix_names.py TK_MTB_Oulu
 
-# Create the gmapsupp map file, NOTE THE MAPNAME HAS TO UNIQUE
+# Create the gmapsupp map file, NOTE THE MAPNAME HAS TO UNIQUE, FAMILY ID IS ALSO UNIQUE
 
-java -jar -Xmx1000m mkgmap.jar --max-jobs --gmapsupp --latin1 --tdbfile --nsis --mapname=88880002 --family-id=8888 --series-name="OSM MTB Oulu region" --style-file=TK/ --precomp-sea=sea.zip --generate-sea --route --drive-on=detect,right --process-destination --process-exits --index --bounds=bounds.zip --location-autofill=is_in,nearest --x-split-name-index --housenumbers --remove-ovm-work-files -c template.args TK.typ
+java -jar -Xmx1000m mkgmap.jar --max-jobs --gmapsupp --latin1 --tdbfile --nsis --mapname=88880002 --family-id=8801 --series-name="TK_MTB_Oulu" --style-file=TK/ --precomp-sea=sea.zip --generate-sea --route --drive-on=detect,right --process-destination --process-exits --index --bounds=bounds.zip --location-autofill=is_in,nearest --x-split-name-index --housenumbers --remove-ovm-work-files -c template.args TK.typ
 
 # copy the map file to /var/www for downloading
-mv gmapsupp.img OSM_TK_MTB_map_Oulu.img
-sudo chown www-data:www-data OSM_TK_MTB_map_Oulu.img
-sudo mv -f OSM_TK_MTB_map_Oulu.img /var/www
+mv gmapsupp.img tk_oulu.img
+sudo chown www-data:www-data tk_oulu.img
+sudo mv -f tk_oulu /var/www
 
 #################################
 
